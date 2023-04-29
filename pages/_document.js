4052addbf90901c9585480d830a1068ef96f8c72
MuffinTheGhostHumper <130828234+MuffinTheGhostHumper@users.noreply.github.com>
2023-04-30 00:55:16 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'p',
+    title: 'Shinken-Cup-Tournament',
     description: 'Tournament',
     image: 'https://shinken-cup-tournament.vercel.app/images/katana.jpg'
   }
